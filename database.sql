57e1f8f2da67f8f7baed6b0ebfba306a480f796b
Sharifa Simon <52328937+moose35@users.noreply.github.com>
2019-10-15 16:37:14 -0500

    Finish questions

--- database.sql
+++ database.sql
@@ -1,5 +1,5 @@
 --How do you get all users from Chicago?
-SELECT * FROM "accounts"
+SELECT "username" FROM "accounts"
 WHERE "city" ILIKE 'Chicago';
 
 --How do you get all users with usernames that contain the letter a?
@@ -21,12 +21,29 @@ AND "transactions_attempted" =0;
 SELECT * FROM "accounts"
 WHERE "transactions_attempted" >=9;
 
-How do you get the username and account balance of the 3 users with the highest balances, sorted highest to lowest balance? NOTE: Research LIMIT
+--How do you get the username and account balance of the 3 users with the highest balances, sorted highest to lowest balance? NOTE: Research LIMIT
+SELECT "username", "account_balance" FROM "accounts"
+ORDER BY "account_balance" desc
+LIMIT 3 OFFSET 0;
 
-How do you get the username and account balance of the 3 users with the lowest balances, sorted lowest to highest balance?
+--How do you get the username and account balance of the 3 users with the lowest balances, sorted lowest to highest balance?
+SELECT "username", "account_balance" FROM "accounts"
+ORDER BY "account_balance" asc
+LIMIT 3 OFFSET 7;
 
-How do you get all users with account balances that are more than $100?
+--How do you get all users with account balances that are more than $100?
+SELECT "username" FROM "accounts"
+WHERE "account_balance" >100;
 
-How do you add a new account?
+--How do you add a new account?
+INSERT INTO "accounts"(user_id,
+    username,
+    city,
+    transactions_completed,
+    transactions_attempted)
+VALUES(11,'Sharifa', 'Minneapolis', '0','0');
 
-The bank is losing money in Miami and Phoenix and needs to unload low transaction customers: How do you delete users that reside in miami OR phoenix and have completed fewer than 5 transactions.
+--The bank is losing money in Miami and Phoenix and needs to unload low transaction customers: How do you delete users that reside in miami OR phoenix and have completed fewer than 5 transactions.
+ELECT * FROM "accounts"
+WHERE "city" = 'phoenix'AND "transactions_completed"<5
+OR "city" = 'miami' AND "transactions_completed"<5;
